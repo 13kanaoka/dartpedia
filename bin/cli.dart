b10f9003fd016b6c46f8ea02b16ccdc858003dc8
Jedediah <mrohde213@gmail.com>
2026-07-07 17:15:42 +0900

    feat(cli): add version command

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,3 +1,13 @@
+const version = '0.0.1';
+
 void main(List<String> arguments) {
-  print('Hello, Dart!');
+  if (arguments.isEmpty) {
+    print('Hello, Dart!');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  }
+}
+
+void printUsage() {
+  print("The following commands are valid: 'help', 'version', 'search <ARTICLE TITLE>'");
 }
